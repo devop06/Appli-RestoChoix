047a38a290b553d1ca4a1296a8541787d809d39c
Villanova <tonyvillanova@hotmail.fr>
2016-04-04 21:39:36 +0200

    ajout classe test

--- ChoixRestoTests/DalTest.cs
+++ ChoixRestoTests/DalTest.cs
@@ -5,50 +5,6 @@ using System.Data.Entity;
 
 namespace ChoixRestoTest
 {
-    /* [TestClass]
-     public class DalTest
-     {
-         [TestInitialize]
-         public void Purge_avantTest()
-         {
-             IDatabaseInitializer<BddContext> init = new DropCreateDatabaseAlways<BddContext>();
-             Database.SetInitializer(init);
-             init.InitializeDatabase(new BddContext());
-         }
-
-         [TestMethod]
-         public void CreerRestaurant_AvecNouveauRestaurant_RetrouveBienLeResto()
-         {
-             using (IDal dal = new Dal() )
-             {
-                 dal.CreerResto("Mcdonald's", "0160589788" );
-                 System.Collections.Generic.List<Restaurant> listeResto = dal.ObtenirListeResto();
-                 Assert.IsNotNull(listeResto);
-                 Assert.AreEqual(1, listeResto.Count);
-                 Assert.AreEqual("Mcdonald's", listeResto[0].Nom);
-                 Assert.AreEqual("0160589788", listeResto[0].Telephone);
-                 dal.Dispose();
-             }
-         }
-
-
-         [TestMethod]
-         public void ModifierRestaurant_CreationDUnNouveauRestaurantEtChangementNomEtTelephone_LaModificationEstCorrecteApresRechargement()
-         {
-             using (IDal dal = new Dal())
-             {
-                 dal.CreerResto("Mcdonald's", "0160589788");
-                 dal.ModifierLesRestos(1, "macdonald 2", null );
-                 List<Restaurant> restos = dal.ObtenirListeResto();
-                 Assert.IsNotNull(restos);
-                 Assert.AreEqual(1, restos.Count);
-                 Assert.AreEqual("macdonald 2", restos[0].Nom);
-                 Assert.IsNull(restos[0].Telephone);
-             }
-         }
-     }*/
-
-        // test
     [TestClass]
     public class DalTests
     {
@@ -60,7 +16,6 @@ namespace ChoixRestoTest
             IDatabaseInitializer<BddContext> init = new DropCreateDatabaseAlways<BddContext>();
             Database.SetInitializer(init);
             init.InitializeDatabase(new BddContext());
-
             dal = new Dal();
         }
 

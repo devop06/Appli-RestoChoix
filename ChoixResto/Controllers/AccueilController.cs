275e12bc58c62017823899219312907a39ce9bbf
Villanova <tonyvillanova@hotmail.fr>
2016-04-17 11:12:55 +0200

    view model concept

---
+++ ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["resto"] = new ChoixResto.Models.Restaurant { Nom = "Beuverie", Telephone = "0645879878" }; 
+            return View(new Models.Utilisateur { Prenom = "user", MotDePasse = "xxxxxxxyyyxxxx" });
+        }
+
+        public ActionResult Presentation()
+        {
+            ChoixResto.ViewModels.PresentationViewModel viewModel =
+                new ViewModels.PresentationViewModel { Date = new DateTime(2015, 12, 12), Message = "C'est un restaurant typique c#", Restaurant = new Models.Restaurant { Nom = "Beuverie", Telephone = "0645879878" } };
+
+            return View("Presentation", viewModel);
+        }
+    }
+}
---
+++ ChoixResto/ViewModels/PresentationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoixResto.ViewModels
+{
+    public class PresentationViewModel
+    {
+        public string Message { get; set; }
+        public Models.Restaurant Restaurant {get;set;}
+        public DateTime Date { get; set; }
+    }
+}
